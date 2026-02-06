16117265a921c067524d7312972ca9d48c331c00
Jakub Krać <jakubkrac0123@gmail.com>
2026-02-07 00:55:40 +0100

    feat(Core): struktura bazowa
    feat(Core): inicjalna konfiguracja struktury plików projektu
    feat(UI): utworzenie wstępnego okna instalacyjnego InstallWindow
    refactor(Logic): dostosowanie i optymalizacja głównych metod w klasie App.cs

---
+++ Scripts/App.xaml.cs
@@ -0,0 +1,30 @@
+﻿using System.Configuration;
+using System.Data;
+using System.Windows;
+using Neo4J.Properties;
+
+namespace Neo4J
+
+{
+    public partial class App : Application
+    {
+        public void ShowWindow(Window windowObject)
+        {
+            Application.Current.MainWindow = windowObject;
+            windowObject.Show();
+        }
+
+        public void CloseWindow(Window windowObject)
+        {
+            windowObject.Close();
+        }
+        protected override void OnStartup(StartupEventArgs e)
+        {
+            base.OnStartup(e);
+            if (Settings.Default.IsInstalled == false)
+            {
+                ShowWindow(new InstallWindow());
+            }
+        }
+    }
+}
---
+++ Scripts/InstallWindow.xaml.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+
+
+namespace Neo4J
+{
+    /// <summary>
+    /// Logika interakcji dla klasy LoginWindow.xaml
+    /// </summary>
+    public partial class InstallWindow : Window
+    {
+        public InstallWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void BtnInstall_Click(object sender, RoutedEventArgs e)
+        {
+            ((App)Application.Current).ShowWindow(new MainWindow());
+            ((App)Application.Current).CloseWindow(this);
+        }
+    }
+}
+
