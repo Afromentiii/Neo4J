b0b169adcd63767938036053149711b82874fcc7
Jakub Krać <jakubkrac0123@gmail.com>
2026-02-07 02:11:31 +0100

    Setup + InstallWindow

---
+++ Scripts/App.xaml.cs
@@ -0,0 +1,25 @@
+﻿using System.Configuration;
+using System.Data;
+using System.Windows;
+using Neo4J.Properties;
+
+namespace Neo4J
+
+{
+    public partial class App : Application
+    {
+        private void CreateShowWindow(Window windowObject)
+        {
+            Application.Current.MainWindow = windowObject;
+            windowObject.Show();
+        }
+        protected override void OnStartup(StartupEventArgs e)
+        {
+            base.OnStartup(e);
+            if (Settings.Default.IsInstalled == false)
+            {
+                CreateShowWindow(new InstallWindow());
+            }
+        }
+    }
+}
---
+++ Scripts/InstallWindow.xaml.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+
+namespace Neo4J
+{
+    /// <summary>
+    /// Logika interakcji dla klasy LoginWindow.xaml
+    /// </summary>
+    public partial class InstallWindow : Window
+    {
+        public InstallWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void BtnInstall_Click(object sender, RoutedEventArgs e)
+        {
+            MainWindow main = new MainWindow();
+            App.Current.MainWindow = main;
+            main.Show();
+            this.Close();
+        }
+    }
+}
+
