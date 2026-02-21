b2ed56b850c18c2f98a584cd8e7802828e8591f9
Jakub Krać <jakubkrac0123@gmail.com>
2026-02-21 21:13:39 +0100

    feat(DB): integracja z Neo4j
    feat(DB): implementacja sterownika sprzęgającego bazę (Neo4jDriver)
    feat(Logic): dodanie głównego kodu połączeniowego obsługującego autoryzację do bazy

---
+++ Scripts/Neo4jDriver.cs
@@ -0,0 +1,29 @@
+﻿using Neo4j.Driver;
+using System;
+using System.Threading.Tasks;
+
+namespace Neo4J
+{
+    public class Neo4jDriver : IDisposable
+    {
+        private static readonly Lazy<Neo4jDriver> _instance =
+            new Lazy<Neo4jDriver>(() => new Neo4jDriver());
+
+        public static Neo4jDriver Instance => _instance.Value;
+
+        public IDriver Driver { get; private set; }
+
+        public async Task Initialize(string uri, string user, string password)
+        {
+            Driver?.Dispose();
+
+            Driver = GraphDatabase.Driver(uri, AuthTokens.Basic(user, password));
+            await Driver.VerifyConnectivityAsync();
+        }
+
+        public void Dispose()
+        {
+            Driver?.Dispose();
+        }
+    }
+}
