535b6c5cb8e3ab977117f4236a8b1e0b0e501d07
Jakub Krać <jakubkrac0123@gmail.com>
2026-02-21 21:13:39 +0100

    Adding Neo4jDriver and connection code

--- Scripts/InstallWindow.xaml.cs
+++ Scripts/InstallWindow.xaml.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Security.Policy;
 using System.Text;
 using System.Windows;
 using System.Windows.Controls;
@@ -9,13 +10,10 @@ using System.Windows.Input;
 using System.Windows.Media;
 using System.Windows.Media.Imaging;
 using System.Windows.Shapes;
-
+using Neo4j.Driver;
 
 namespace Neo4J
 {
-    /// <summary>
-    /// Logika interakcji dla klasy LoginWindow.xaml
-    /// </summary>
     public partial class InstallWindow : Window
     {
         public InstallWindow()
@@ -23,10 +21,43 @@ namespace Neo4J
             InitializeComponent();
         }
 
-        private void BtnInstall_Click(object sender, RoutedEventArgs e)
+        private async void BtnInstall_Click(object sender, RoutedEventArgs e)
         {
-            ((App)Application.Current).ShowWindow(new MainWindow());
-            ((App)Application.Current).CloseWindow(this);
+            string connectionUrl = TxtUrl.Text;
+            string userName = TxtUser.Text;
+            string password = TxtPassword.Password;
+
+            if (string.IsNullOrWhiteSpace(connectionUrl))
+            {
+                MessageBox.Show("Please enter a valid Connection URL (for example, bolt://localhost:7687)", "Validation Error");
+                return;
+            }
+
+            if (string.IsNullOrWhiteSpace(userName))
+            {
+                MessageBox.Show("Username cannot be empty.", "Validation Error");
+                return;
+            }
+
+            if (string.IsNullOrWhiteSpace(password))
+            {
+                MessageBox.Show("Password cannot be empty.", "Validation Error");
+                return;
+            }
+
+            try
+            {
+                BtnInstall.IsEnabled = false;
+                await Neo4jDriver.Instance.Initialize(connectionUrl, userName, password);
+
+                ((App)Application.Current).ShowWindow(new MainWindow());
+                ((App)Application.Current).CloseWindow(this);
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show($"Error: {ex.Message}");
+                BtnInstall.IsEnabled = true;
+            }
         }
     }
 }
